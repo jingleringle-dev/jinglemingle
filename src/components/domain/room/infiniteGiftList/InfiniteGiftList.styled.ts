29ea32bb306dbdc1e807eec61870c199a7f62335
zinnli <102575747+zinnli@users.noreply.github.com>
2023-12-11 00:12:22 +0900

    style: 내방 리스트 스크린 이슈 수정 (#27) (#28)

--- src/components/domain/room/infiniteGiftList/InfiniteGiftList.styled.ts
+++ src/components/domain/room/infiniteGiftList/InfiniteGiftList.styled.ts
@@ -2,8 +2,16 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 85%;
-  height: 65%;
+  height: 75vh;
   background-color: rgba(255, 255, 255, 0.9);
   margin: 0 auto;
   overflow-y: hidden;
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    padding: 30px 0;
+  }
 `;
--- src/components/layout/roomLayout/RoomLayout.styled.ts
+++ src/components/layout/roomLayout/RoomLayout.styled.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.section`
+export const Container = styled.div`
   width: 100%;
   height: 100vh;
   position: relative;
